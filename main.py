03fe724b12a52ba74730ca551070620203eb6feb
Алексей Чаадаев <104098139+Tsygaika@users.noreply.github.com>
2024-05-27 18:55:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ async def get_code(session, url):
         return await response.text()    #возвращаем содержимое ответа в виде текста
 
 
-async def get_links(html):
+async def get_links(html, url):
     soup = BeautifulSoup(html, 'html.parser')   #создаем объект для парсинга сайта
-    links = [a.get('href') for a in soup.find_all('a', href = True) if 'http' in a.get('href')]  #и получаем все html теги <a> с атрибутом href,
-    return links                                                                                 #в которых содержатся http(s) ссылки
+    links = [a.get('href') for a in soup.find_all('a', href = True)]  #и получаем все html теги <a> с атрибутом href,
+    for i in range(len(links)):
+        if 'http' not in links[i]:#если ссылка относительная, то сделаем ее абсолютной
+            links[i] = url + links[i]
+            
+    return links                                                                                
 
 
 async def save_to_file(links):
@@ -22,9 +26,13 @@ async def save_to_file(links):
 
 
 async def process_url(session, url):    #функция обработки каждой ссылки
-    html_code = await get_code(session, url)    #получаем html-код сайта 
-    links = await get_links(html_code)  #получаем ссылки из кода
-    await save_to_file(links)
+    try:
+        html_code = await get_code(session, url)    #получаем html-код сайта
+        links = await get_links(html_code, url)  #получаем ссылки из кода
+        await save_to_file(links)
+        
+    except Exception:
+        print(f'Не удалось получить доступ к сайту: {url}')
         
 
 async def main(urls):
